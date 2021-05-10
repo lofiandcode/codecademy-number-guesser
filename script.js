0385b344c81c0b8c39dab6d052b314b22dba761a
lofiandcode <lofiandcode@gmail.com>
2021-05-10 16:36:55 -0700

    add function that returns the absolute difference between two numbers

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 const generateTarget = () => {
-    return Math.floor(Math.random() * 10);
-  }
+  return Math.floor(Math.random() * 10);
+}
+
+const difference = (num1, num2) => {
+  return Math.abs(num1 - num2);
+}
